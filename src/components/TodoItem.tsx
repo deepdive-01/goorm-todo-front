616c6f5c71c6d60e8c287e413c8373df959da6d7
Chaewon Lee <codenamewont@gmail.com>
2026-03-24 21:28:30 +0900

    feat: TodoItem props 추가

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,16 +1,39 @@
 import { useState } from 'react';
 import Text from './Text';
 
-export default function TodoItem() {
+type Category = 'focus' | 'quick' | 'plan' | 'drop' | 'later';
+
+type TodoItemProps = {
+  text: string;
+  category: Category;
+};
+
+export default function TodoItem({ text, category }: TodoItemProps) {
   const [checked, setChecked] = useState(false);
+
+  const categoryBorderMap = {
+    focus: 'border-primary',
+    quick: 'border-quick',
+    plan: 'border-plan',
+    drop: 'border-drop',
+    later: 'border-primary-light',
+  };
+  const categoryBgMap = {
+    focus: 'bg-primary',
+    quick: 'bg-quick',
+    plan: 'bg-plan',
+    drop: 'bg-drop',
+    later: '',
+  };
 
   return (
     <li className="w-full rounded-4xl p-6 flex gap-5 bg-white shadow-sm items-center">
       <button
+        disabled={category === 'later'}
         onClick={() => setChecked((prev) => !prev)}
-        className={`w-5.5 h-5.5 border-plan border-2 rounded-full ${checked ? 'bg-plan' : ''}`}
+        className={`w-5.5 h-5.5 border-2 rounded-full ${categoryBorderMap[category]} ${checked ? categoryBgMap[category] : ''}`}
       ></button>
-      <Text className={checked ? 'text-gray-text line-through' : ''}>아침 차 마시며 독서하기</Text>
+      <Text className={checked ? 'text-gray-text line-through' : ''}>{text}</Text>
     </li>
   );
 }
