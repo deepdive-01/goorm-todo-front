18d87a5b6dac11a98fcc7f83ef4cba8f327caa63
Chaewon <codenamewont@gmail.com>
2026-03-24 17:00:37 +0900

    feat: header 공통 컴포넌트

--- src/pages/today/index.tsx
+++ src/pages/today/index.tsx
@@ -1,9 +1,11 @@
 import TodoItem from '@/components/TodoItem';
+import Header from '@/components/Header';
 
 export default function TodayPage() {
   return (
     <div className="w-full h-full flex items-center justify-center">
       <div className="w-93.75 bg-gray-ui">
+        <Header />
         <TodoItem />
       </div>
     </div>
