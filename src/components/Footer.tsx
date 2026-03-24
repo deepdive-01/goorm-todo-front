0d11402ab4a2057a9da3611c9c3ae0a075423d3d
Chaewon <codenamewont@gmail.com>
2026-03-25 00:51:58 +0900

    feat: footer 공통 컴포넌트

---
+++ src/components/Footer.tsx
@@ -0,0 +1,57 @@
+import { useNavigate, useLocation } from 'react-router-dom';
+import TodayIcon from '@/assets/today-icon.svg?react';
+import CalendarIcon from '@/assets/calendar-icon.svg?react';
+import FriendsIcon from '@/assets/friends-icon.svg?react';
+import SettingsIcon from '@/assets/settings-icon.svg?react';
+import Text from './Text';
+
+const tabs = [
+  { key: 'today', label: '오늘', Icon: TodayIcon, path: '/today' },
+  { key: 'calendar', label: '캘린더', Icon: CalendarIcon, path: '/calendar' },
+  { key: 'friends', label: '친구', Icon: FriendsIcon, path: '/friends' },
+  { key: 'settings', label: '설정', Icon: SettingsIcon, path: '/settings' },
+] as const;
+
+export default function Footer() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  return (
+    <footer className="w-full h-18 flex flex-row bg-white">
+      {tabs.map((tab) => {
+        const isActive = location.pathname === tab.path;
+
+        return (
+          <FooterButton
+            key={tab.key}
+            label={tab.label}
+            Icon={tab.Icon}
+            onClick={() => navigate(tab.path)}
+            isActive={isActive}
+          />
+        );
+      })}
+    </footer>
+  );
+}
+
+type FooterButtonProps = {
+  label: string;
+  Icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+  onClick?: () => void;
+  isActive: boolean;
+};
+
+function FooterButton({ label, Icon, onClick, isActive }: FooterButtonProps) {
+  return (
+    <button
+      onClick={onClick}
+      className="flex-1 flex flex-col gap-1 items-center justify-center cursor-pointer group"
+    >
+      <Icon className={`group-hover:scale-105 ${isActive ? 'text-primary' : 'text-black'}`} />
+      <Text className={`group-hover:scale-105 ${isActive ? 'text-primary' : 'text-black'}`}>
+        {label}
+      </Text>
+    </button>
+  );
+}
--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -1,3 +1,14 @@
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+
 export default function SettingsPage() {
-  return <div>설정 페이지</div>;
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      <div className="w-93.75 bg-gray-ui">
+        <Header />
+        <main></main>
+        <Footer />
+      </div>
+    </div>
+  );
 }
