359531fa4440d49030a4899af09fe872bcf2a0f1
Chaewon Lee <codenamewont@gmail.com>
2026-03-25 10:48:30 +0900

    feat: footer 공통 컴포넌트

--- src/pages/friends/index.tsx
+++ src/pages/friends/index.tsx
@@ -1,3 +1,14 @@
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+
 export default function FriendsPage() {
-  return <div>친구 페이지</div>;
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      <div className="w-93.75 bg-gray-ui">
+        <Header />
+        <main></main>
+        <Footer />
+      </div>
+    </div>
+  );
 }
