1ff89c07684e1d1fdbb7bf9e947e8a3f7bd8c630
조성민 <127915024+tjdalsquf8@users.noreply.github.com>
2026-03-27 02:26:45 +0900

    feat: 친구투두페이지 구현

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import TodayPage from "@/pages/today";
 import CalendarPage from "@/pages/calendar";
 import FriendsPage from "@/pages/friends";
 import SettingsPage from "@/pages/settings";
-
+import FriendsTodoPage from "@/pages/friendstodo"
 export default function App() {
   return (
     <Routes>
@@ -11,6 +11,7 @@ export default function App() {
       <Route path="/today" element={<TodayPage />} />
       <Route path="/calendar" element={<CalendarPage />} />
       <Route path="/friends" element={<FriendsPage />} />
+      <Route path="/friendstodo" element={<FriendsTodoPage />} />
       <Route path="/settings" element={<SettingsPage />} />
     </Routes>
   );
