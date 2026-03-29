3e4c04b8655fbaadc962b25eb4696de2694e4994
조성민 <127915024+tjdalsquf8@users.noreply.github.com>
2026-03-30 00:49:23 +0900

    feat: Friend페이지 구현 (#16)
    * feat: Friend페이지 구현
    
    * Update src/pages/friends/components/AddFriendCard.tsx
    
    Co-authored-by: Chaewon Lee <codenamewont@gmail.com>
    
    * fix: 버튼공통컴포넌트 사용 / 아이콘 추가
    
    ---------
    
    Co-authored-by: Chaewon Lee <codenamewont@gmail.com>

---
+++ src/pages/friends/components/AddFriendCard.tsx
@@ -0,0 +1,32 @@
+import Text from '@/components/Text';
+import Button from '@/components/Button';
+import SmileIcon from '@/assets/smile-icon.svg?react';
+
+export default function AddFriendCard() {
+  const handleFindFriend = () => {
+    console.log('친구 찾기');
+  };
+
+  return (
+    <div className="w-full h-[307px] border border-dashed border-primary rounded-[32px] flex flex-col items-center justify-center gap-6 px-6">
+      <SmileIcon className="w-[52px] h-[52px]" />
+
+      <div className="flex flex-col items-center gap-2">
+        <Text variant="title" className="text-black">
+          친구를 추가해보세요!
+        </Text>
+        <Text variant="body" className="text-black text-center">
+          친구와 할 일을 함께할 수 있습니다.
+        </Text>
+      </div>
+
+      <Button
+        variant="primary"
+        onClick={handleFindFriend}
+        className="w-[134.5px]"
+      >
+        친구 찾기
+      </Button>
+    </div>
+  );
+}
