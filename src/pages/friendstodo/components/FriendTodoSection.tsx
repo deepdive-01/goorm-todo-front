dac1f2fb89708391fc33e525d4c2877a25b74c05
조성민 <127915024+tjdalsquf8@users.noreply.github.com>
2026-03-30 00:55:20 +0900

    feat: 친구투두페이지 구현 (#17)
    * feat: 친구투두페이지 구현
    
    * fix: LeftIcon 사용

---
+++ src/pages/friendstodo/components/FriendTodoSection.tsx
@@ -0,0 +1,30 @@
+import Text from '@/components/Text';
+import AvatarIcon from '@/assets/avatar-icon.svg?react';
+import LeftIcon from '@/assets/leftarrow-icon.svg?react'
+
+type FriendTodoSectionProps = {
+  friendName: string;
+  onBack?: () => void;
+};
+
+export default function FriendTodoSection({ friendName, onBack }: FriendTodoSectionProps) {
+  return (
+    <div className="w-full h-12 bg-white rounded-lg px-4 flex items-center gap-3">
+      {/* 뒤로가기 버튼 */}
+      <button onClick={onBack} className="w-6 h-6 flex items-center justify-center text-black text-xl">
+        <LeftIcon />
+      </button>
+
+
+      {/* 아바타 */}
+      <div className="w-10 h-10 rounded-full border-2 border-primary flex items-center justify-center overflow-hidden">
+        <AvatarIcon className="w-full h-full" />
+      </div>
+
+      {/* 친구 이름 */}
+      <Text variant="body" className="text-black font-semibold">
+        {friendName}
+      </Text>
+    </div>
+  );
+}
